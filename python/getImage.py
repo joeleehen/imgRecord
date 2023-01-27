a517c57bb97763973774c94aa3f80d1ea88a2f29
Joseph Hendrix <joeylhendrix@gmail.com>
2023-01-26 20:17:25 -0600

    getImg.py fixes"

--- python/getImage.py
+++ python/getImage.py
@@ -1,16 +1,15 @@
 import requests
 
-def getImg(search):
+def getImage(search):
     payload = {'barcode' : str(search), 'token' : 'uEfsYPggGDjMXoVbgYvPEFLYtUnRtucFQibSkSqi'}
     r = requests.get('https://api.discogs.com/database/search', params=payload)
     data = r.json()
 
-    # attempt to extract image URL from json dict
-    try:
-        results = data[1]
-        url = results[0].get('cover_image')
-        return url
-    except:
-        print('an error occurred')
-        return 
 
+    results = data['results']
+    # print(results)
+    url = results[0].get('cover_image')
+    if url:
+        return url
+    else:
+        return 0
