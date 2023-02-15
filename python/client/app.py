7b4b36fe34c3b4dd8d64ccb2bbbc45d427a8e388
Joseph Hendrix <joeylhendrix@gmail.com>
2023-02-15 14:20:48 -0600

    uhhh flask nonsense

--- python/client/app.py
+++ python/client/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import displayArt
+from displayArt import displayArt
 from getImage import *
 
 app = Flask(__name__)
--- python/client/displayArt.py
+++ python/client/displayArt.py
@@ -5,20 +5,23 @@ from PIL import Image
 import requests
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
-# matrix configuration
-options = RGBMatrixOptions()    # amazing brother
-options.rows = 32
-options.cols = 32
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = 'adafruit-hat'
-options.gpio_slowdown = 4
+def displayArt(url):
+    # matrix configuration
+    options = RGBMatrixOptions()    # amazing brother
+    options.rows = 32
+    options.cols = 32
+    options.chain_length = 1
+    options.parallel = 1
+    options.hardware_mapping = 'adafruit-hat'
+    options.gpio_slowdown = 4
 
-matrix = RGBMatrix(options = options)    # pop off (only on occasion) brother
+    matrix = RGBMatrix(options = options)    # pop off (only on occasion) brother
 
-url = sys.argv[1]
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
-img.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+    url = sys.argv[1]
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 
-matrix.SetImage(img.convert("RGB"))
+    matrix.SetImage(img.convert("RGB"))
+
+    return
