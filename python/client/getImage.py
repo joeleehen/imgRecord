f37acffdb51eb8d43c58170e5ac689139a1c2f3e
joeleehen <joeylhendrix@gmail.com>
2023-03-16 19:13:46 +0000

    functional barcode search!

--- python/client/getImage.py
+++ python/client/getImage.py
@@ -1,6 +1,7 @@
 import requests
 
 def getImageBar(search):
+    print(search)
     try:
         payload = {'barcode' : str(search), 'token' : 'uEfsYPggGDjMXoVbgYvPEFLYtUnRtucFQibSkSqi'}
         r = requests.get('https://api.discogs.com/database/search', params=payload)
@@ -15,10 +16,12 @@ def getImageBar(search):
         else:
             return
     except:
-            return
+            return 'https://i.discogs.com/a6wAohfJ1KTLXijU79CjHu7nRUYqVC8mES2O4VnlbIA/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM5MTQ5/MTQtMTM5MjQzNTE1/Ny0yODEwLmpwZWc.jpeg'
+
 def getImageTxt(album, artist):
     if album == "" or artist == "":
         return
+    print(album, artist)
     payload = {'release_title' : str(album), 'artist' : str(artist), 'token' : 'uEfsYPggGDjMXoVbgYvPEFLYtUnRtucFQibSkSqi'}
     r = requests.get('https://api.discogs.com/database/search', params=payload)
     data = r.json()
